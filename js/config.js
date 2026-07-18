09e997b1be9e5097bf219ac01eac86e579f08818
Andre-101 <72985476+Andre-101@users.noreply.github.com>
2026-07-18 18:54:18 -0500

    feat: add public application configuration

---
+++ js/config.js
@@ -0,0 +1,6 @@
+export const APP_CONFIG = Object.freeze({
+  API_URL: "https://script.google.com/macros/s/AKfycby4EwzE9jCyu358Uc9SqZ_qOCME7iproCj0L3S2-0k6Y4K7m68DSuz__JxvyUmTikKP/exec",
+  OSRM_BASE_URL: "https://router.project-osrm.org",
+  DEFAULT_NEAREST_LIMIT: 10,
+  DATASET_VERSION: "2026-07-mvp-01",
+});
