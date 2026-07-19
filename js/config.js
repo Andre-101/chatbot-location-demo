78696106ac199623b410f8b6ecdd496ce1b6e74e
Andre-101 <72985476+Andre-101@users.noreply.github.com>
2026-07-18 19:20:31 -0500

    fix: point frontend to the working Apps Script deployment

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = Object.freeze({
-  API_URL: "https://script.google.com/macros/s/AKfycby4EwzE9jCyu358Uc9SqZ_qOCME7iproCj0L3S2-0k6Y4K7m68DSuz__JxvyUmTikKP/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbzy2zHuC2RtzR93N9Svi5SfO1vrgKBzpbbXQM9-R_basgpe6zdxw7lwGjSLBXq3VmIs/exec",
   OSRM_BASE_URL: "https://router.project-osrm.org",
   DEFAULT_NEAREST_LIMIT: 10,
   DATASET_VERSION: "2026-07-mvp-01",
